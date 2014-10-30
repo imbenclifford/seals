6f17cedd978e5cfad487be158b6da4d55b366bd0
imbenclifford <imbenclifford@gmail.com>
2014-10-30 15:25:57 +0000

    fb-authy1

--- index.js
+++ index.js
@@ -51,17 +51,6 @@ var options = {
     }]
 };
 
-server.pack.register({
-    plugin: require('good'),
-    options: options
-}, function (err) {
-
-   if (err) {
-      console.log(err);
-      return;
-   }
-});
-
 server.views({
     engines: { jade: require('jade') },
     path: './jade'
@@ -69,15 +58,69 @@ server.views({
 
 server.pack.register([
   {
+    plugin: require('good'),
+    options: options
+  },
+  {
     plugin: require( 'hapi-mongodb'),
     options: dbOpts
   },
-  { plugin: require("./plugins/comments") }],
+  { plugin: require('bell') },
+    { plugin: require('hapi-auth-cookie') },
+    { plugin: require('./plugins/auth')}],
  function (err) {
     if (err) {
         console.error(err);
         throw err;
     }
+  server.route([{
+        path: '/myprofile',
+        method: 'GET',
+        config: {
+            auth: 'session',
+            handler: function(request, reply) {
+                reply('<html><head><title>Login page</title></head><body><h3>Welcome '
+                  + JSON.stringify(request.auth.credentials, null, 4)
+                  + '!</h3><br/><form method="get" action="/logout">'
+                  + '<input type="submit" value="Logout">'
+                  + '</form></body></html>');
+            }
+        }
+    }, {
+        path: '/',
+        method: 'GET',
+        config: {  // try with redirectTo disabled makes isAuthenticated usefully available
+            auth: {
+                strategy: 'session',
+                mode: 'try'
+            },
+            plugins: { 'hapi-auth-cookie': { redirectTo: false } }
+        },
+        handler: function(request, reply) {
+            reply.view('index', {
+                auth: JSON.stringify(request.auth),
+                session: JSON.stringify(request.session),
+                isLoggedIn: request.auth.isAuthenticated
+            });
+        }
+    }, {
+        path: '/{path*}',
+        method: 'GET',
+        handler: {
+            directory: {
+                path: './public',
+                listing: false,
+                index: true
+            }
+        }
+    }]);
+    server.start(function(err) {
+        if (err) {
+            console.log('error message ' + err);
+        }
+        console.log('Hapi server started @ ' + server.info.uri);
+        console.log('server started on port: ', server.info.port);
+    });
 });
 
 server.route({
@@ -132,18 +175,6 @@ server.route( {
    "method"  : "POST",
    "path"    : "/editsub/{id}",
    "handler" : handlers.editHandler
-});
-
-server.route( {
-  method : "GET",
-  path :  "/{param*}",
-  handler :  {
-    directory: {
-      path: "./public",
-      listing: false,
-      index: false
-    }
-  }
 });
 
 server.route({
